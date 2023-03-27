91fb46f46f3ac8e5964e3e0eb7b636cc5efba74d
PosteR <poster.aws@gmail.com>
2023-03-27 03:36:07 -0400

    First commit

---
+++ Python-app/main.py
@@ -0,0 +1,10 @@
+import calendar
+print ('********************')
+print ('Welcom to my calandr\n')
+
+year = int(input ('Entre the year:'))
+month = int(input ('Entre the month:'))
+
+print(calendar.month(year, month))
+
+print('Have a hice day')
