84c33a4d79fc122b8b3aa2a87464b9960f14b945
PosteR <poster.aws@gmail.com>
2026-03-05 19:32:52 -0500

    Добавлен скрипт для Q4_fois и внесены изменения для Q4 в Грабер

--- Project LQ/Graber/quotidienne.py
+++ Project LQ/Graber/quotidienne.py
@@ -112,8 +112,8 @@ if response.status_code == 200:
 
                 procedures = [
                     'fill_Q2_stats_order', 'fill_Q2_stats_norder', 'fill_Q2_combo_stats_order', 'fill_Q2_combo_stats_norder',
-                    'fill_Q3_stats_order', 'fill_Q3_stats_norder', 'fill_Q3_combo_stats_order',
-                    'fill_Q4_fois', 'fill_Q4_stats_order', 'fill_Q4_stats_norder', 'fill_Q4_combo_stats_order'
+                    'fill_Q3_stats_order', 'fill_Q3_stats_norder', 'fill_Q3_combo_stats_order', 'fill_Q3_combo_stats_norder',
+                    'fill_Q4_fois', 'fill_Q4_stats_order', 'fill_Q4_stats_norder', 'fill_Q4_combo_stats_order', 'fill_Q4_combo_stats_norder'
                 ]
 
                 for proc in procedures:
